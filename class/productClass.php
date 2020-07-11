2aeb2eb4f828ed70a35140573ed0fdf2eac649f7
Bryan Key <bryansan055@gmail.com>
2020-07-10 22:36:16 -0400

    add function getProducts and insertNewProducts in the class

--- class/productClass.php
+++ class/productClass.php
@@ -5,38 +5,44 @@
         public function getProducts(){
 
             try{
-                $db = getDB();//Aqui llamamos al metodo que hicimos en config, para conectarnos a la base de datos...
+                $db = getDB();
                 $query = $db->prepare("SELECT * FROM product"); 
-                //con $query ejecutamos la consulta a la db...
-                //$query->bindParam("UserAndEmail", $UserAndEmail,PDO::PARAM_STR) ;
-                //$query->bindParam("hash_password", $hash_password,PDO::PARAM_STR) ;
-                //Aqui asignamos los parametros a los valores que usaremos para la consulta.
                 $query->execute();
-                //Aqui ejecutamos el query.
-                $count=$query->rowCount();
-                //Aqui asignamos a cout el metodo de rowCount que devuelve si algo fue afectado, quiere decir
-                //que si el query fallo o sucedio algo inesperado devuelve error
-                $data=$query->fetch(PDO::FETCH_OBJ);
-                //Aqui trae el objeto de la consulta, en este caso, trae id, nombre, contraseña, usuario y demas
+                $data=$query->fetchAll(PDO::FETCH_OBJ); 
                 $db = null;
-                //Hacemos el db nulo para limpiar todo lo que tiene y no crear basura xd
     
-                if($count){
-                    //$_SESSION['uid'] = $data->uid; 
-                    // Aqui guardamos la seccion de la persona que este logeandose
-                    //tomamos del objeto data, el atributo id
+                if($query -> rowCount() > 0){
                     return $data;
                 }else{
                     return false;
                 } 
             }
             catch(PDOException $e) {
-                
                 //echo '{"error":{"text":'. $e->getMessage() .'}}';
             }
 
         }
 
-    }
+        public function insertNewProduct($NameProductInsert,$TypeProductInsert,$PriceProductInsert){
 
+            try{
+                $db = getDB();
+                $query = $db->prepare("INSERT INTO product(name_product,type_product,price_product) values(:name_product,:type_product,:price_product)");
+                $query->bindParam(':name_product',$NameProductInsert,PDO::PARAM_STR);
+                $query->bindParam(':type_product',$TypeProductInsert,PDO::PARAM_STR);
+                $query->bindParam(':price_product',$PriceProductInsert,PDO::PARAM_INT);
+                $query->execute();
+                $db = null;
+                if($query -> rowCount() > 0){
+                    return true;
+                }else{
+                    return false;
+                } 
+            }
+            catch(PDOException $e) {
+                echo '{"error":{"text":'. $e->getMessage() .'}}';
+            }
+        } 
+
+    }
 ?>
